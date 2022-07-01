7ddba763692b48e96c68ad5604b411acad6c434b
alberto londono <albertolondono.londono5@gmail.com>
2022-07-01 11:42:03 -0500

    Modify category-routes.js with put request. Functional

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,8 +44,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  
+  try {
+    // update a category by its `id` value
+    const category = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!category) {
+      res.status(404).json({ message: "No category found with that id" });
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
